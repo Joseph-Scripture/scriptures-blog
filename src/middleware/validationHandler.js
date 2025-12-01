cc6bdbebb9aa77ff28203da0647913ef8aa61b0e
Joseph-Scripture <joescript237@gmail.com>
2025-12-01 19:00:09 +0100

    feat:Corrected password validation to display just first error and saving memory

--- src/middleware/validationHandler.js
+++ src/middleware/validationHandler.js
@@ -3,11 +3,14 @@ const { validationResult } = require("express-validator");
 module.exports = (req, res, next) => {
   const errors = validationResult(req);
 
-  console.log("Validation errors:", errors.array()); // debugging
+  
 
   if (!errors.isEmpty()) {
+    const firstError = errors.array()[0];
     return res.status(400).json({
-      errors: errors.array(),
+      field:firstError.params,
+      message: firstError.msg,
+      value:firstError.value
     });
   }
 
